8c7c8f076189946183a0d27d7ecc5c5245883744
bitbard3 <ansharora3839@gmail.com>
2023-10-25 13:42:08 +0530

    Add and style the summary section of home page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+    const brand = document.querySelector('.brand-text')
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add('bg-dark');
+        brand.classList.add("d-none")
+      } else {
+        navbar.classList.remove('bg-dark');
+        brand.classList.remove("d-none")
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', userScroll);
+  
